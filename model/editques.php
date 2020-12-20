d076c8d629f0fdf3e6c53c2745a1f7225310f981
dgp38 <dgp38@njit.edu>
2020-12-20 08:31:03 -0500

    19th update

--- model/editques.php
+++ model/editques.php
@@ -1,13 +1,17 @@
 <?php
-function editQuestion($questionId, $title, $body, $skills) {
-global $db;
+class editquesDB
+{
+    function editQuestion($questionId, $title, $body, $skills)
+    {
+        global $db;
 
-$query = "UPDATE questions SET title = :title, body = :body, skills = :skills WHERE id = :questionId";
-$statement = $db->prepare($query);
-$statement->bindValue(":title", $title);
-$statement->bindValue(":body", $body);
-$statement->bindValue(":skills", $skills);
-$statement->bindValue(":questionId", $questionId);
-$statement->execute();
-$statement->closeCursor();
+        $query = "UPDATE questions SET title = :title, body = :body, skills = :skills WHERE id = :questionId";
+        $statement = $db->prepare($query);
+        $statement->bindValue(":title", $title);
+        $statement->bindValue(":body", $body);
+        $statement->bindValue(":skills", $skills);
+        $statement->bindValue(":questionId", $questionId);
+        $statement->execute();
+        $statement->closeCursor();
+    }
 }
